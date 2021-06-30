2aa666d22361765ba1bcfeee7493510acc8687e3
jlemaireWolf <39595612+jlemaireWolf@users.noreply.github.com>
2021-06-30 15:59:23 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ToDoList msg="TodoList avec  Vue.js App"/>
+     
+  </div>
+</template>
+
+<script>
+import ToDoList from './components/ToDoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ToDoList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  
+  background-color:#F8F8FA;
+
+}
+</style>
